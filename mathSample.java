93e54e3c07290e82fa10d5e1c5a99d8e93c85f48
4yuuu <jythams05@gmail.com>
2024-09-02 14:54:18 -0700

    number change

--- mathSample.java
+++ mathSample.java
@@ -1,7 +1,7 @@
 public class mathSample {
     public static void main (String[] args) {
-        int num1 = 10;
-        int num2 = 20;
+        int num1 = 5;
+        int num2 = 10;
 
         int sum = num1 + num2;
         
